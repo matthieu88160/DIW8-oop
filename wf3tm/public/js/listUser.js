3f1ee4bdbafcc8a64d361f073f40b8d97694a350
TeamKeepers <team@keepizi.com>
2018-08-08 09:51:11 +0200

    Add jquery version for user list

--- wf3tm/public/js/listUser.js
+++ wf3tm/public/js/listUser.js
@@ -1,4 +1,27 @@
 
+/* Jquery version
+
+$(function(){
+	$.get(
+		'/users.json'
+	).done(function(data){
+		let table = $('<table class="table table-striped"></table>');
+		let head = $('<thead><tr><th>id</th><th>name</th></tr></thead>');
+		let body = $('<tbody></tbody>');
+
+		data.forEach(function(user){
+			body.append($('<tr><td>'+user.id+'</td><td>'+user.name+'</td></tr>'));
+		});
+		
+		table.append(head).append(body);
+		$('#userListContainer').empty().append(table);
+	}).fail(function(){
+		$('#userListContainer').empty().html("An error occured");
+	});
+});
+
+ */
+
 function createTableHead()
 {
 	let tableHead = document.createElement("thead");
